ef3dc636579354a0dc00d6797fcda99b84a8e76c
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-02-25 07:47:33 +0100

    new data works

--- src/pages/PageResource.js
+++ src/pages/PageResource.js
@@ -4,15 +4,12 @@ import { getResource } from '../tools/dataManager';
 export function PageResource() {
 	const { topicId, resourceId } = useParams();
 	const { name, description, id } = getResource({ topicId, resourceId });
-	console.log(name);
-	console.log(description);
-	console.log(id);
 
 	return (
 		<div>
 			<h3>{name}</h3>
 			<p>{description}</p>
-			<a href={`https://ui.dev/${id}`}>Read Post</a>
+			<a href={`https://edwardtanguay.netlify.app/howtos?id=${id}`}>Read Post</a>
 		</div>
 	);
 }
